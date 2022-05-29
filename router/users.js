b51b2dd1545baea217eb5b1985840334018ec71d
LucidJoy <jyotirmoykarmakar02@gmail.com>
2022-05-30 00:34:00 +0530

    CRUD ops w/user

--- router/users.js
+++ router/users.js
@@ -1,7 +1,55 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-  res.send("user route");
+//update user
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt();
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (error) {
+        return res.status(500).json(error);
+      }
+    }
+
+    try {
+      await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("Account has been updated.");
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    return res.status(403).json("You can update only your account.");
+  }
+});
+
+//delete user
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("Account has been deleted.");
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    return res.status(403).json("You can delete only your account.");
+  }
+});
+
+//get a user
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, createdAt, updatedAt, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (error) {
+    return res.status(200).json(error);
+  }
 });
 
 module.exports = router;
