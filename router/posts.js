2e38a8c5f538f06c20b0be09248ede7d19bd49ca
LucidJoy <jyotirmoykarmakar02@gmail.com>
2022-05-30 10:36:31 +0530

    Like, Dislike & Timeline Posts

--- router/posts.js
+++ router/posts.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
+const User = require("../models/User");
 
 //create a post
 router.post("/", async (req, res) => {
@@ -37,6 +38,48 @@ router.delete("/:id", async (req, res) => {
     } else {
       res.status(404).json("You can delete only your post.");
     }
+  } catch (error) {
+    res.status(500).json("Post already deleted.");
+  }
+});
+
+//like & dislike a post
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("Post has been liked.");
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json("Post has been disliked.");
+    }
+  } catch (error) {
+    res.status(500).json("Don't have necessary permissions.");
+  }
+});
+
+//get a post
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json("Post not found.");
+  }
+});
+
+//get timeline post
+router.get("/timeline/all", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
+    const friendPosts = await Promise.all(
+      currentUser.following.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+    res.status(200).json(userPosts.concat(...friendPosts));
   } catch (error) {
     res.status(500).json(error);
   }
