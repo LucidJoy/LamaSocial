53315f370edbb4c5a98da3a6a5dead073b6388f0
LucidJoy <jyotirmoykarmakar02@gmail.com>
2022-05-31 11:07:01 +0530

    Rightbar component

--- frontend/src/components/rightbar/Rightbar.jsx
+++ frontend/src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,34 @@ import React from "react";
 import "./rightbar.css";
 
 const Rightbar = () => {
-  return <div className='rightbar'>Rightbar</div>;
+  return (
+    <div className='rightbar'>
+      <div className='rightbarWrapper'>
+        <div className='birthdayContainer'>
+          <img src='assets/gift.png' alt='' className='birthdayImg' />
+          <span className='birthdayText'>
+            <b>Pola Foster</b> and <b>3 others</b> have their birthday today
+          </span>
+        </div>
+
+        <img className='rightbarAd' src='assets/ad.png' alt='' />
+        <h4 className='rightbarTitle'>Online Friends</h4>
+        <ul className='rightbarFriendList'>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img
+                src='assets/person/3.jpeg'
+                alt=''
+                className='rightbarProfileImg'
+              />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>Lisa Amani</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Rightbar;
