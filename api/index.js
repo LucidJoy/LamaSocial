d690f0ec289568a1b7645dc98f2865128554e690
LucidJoy <jyotirmoykarmakar02@gmail.com>
2022-06-02 10:20:32 +0530

    NodeJS file uploading

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
+const multer = require("multer");
+const path = require("path");
 
 const userRoute = require("./router/users");
 const authRoute = require("./router/auth");
@@ -15,10 +17,30 @@ mongoose.connect(process.env.MONGO_URL, () => {
   console.log("Connected to MongoDB");
 });
 
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
 //middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploaded!");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
--- frontend/src/components/share/Share.jsx
+++ frontend/src/components/share/Share.jsx
@@ -1,23 +1,77 @@
-import React from "react";
+import React, { useContext, useRef, useState } from "react";
 import { PermMedia, Label, Room, EmojiEmotions } from "@mui/icons-material";
+import axios from "axios";
 
+import { AuthContext } from "../../context/AuthContext";
 import "./share.css";
 
 const Share = () => {
+  const { user } = useContext(AuthContext);
+  const [file, setFile] = useState(null);
+  const desc = useRef();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const newPost = {
+      userId: user._id,
+      desc: desc.current.value,
+    };
+    if (file) {
+      const data = new FormData();
+      const fileName = Date.now() + file.name;
+      data.append("file", file);
+      data.append("name", fileName);
+      newPost.image = fileName;
+      try {
+        await axios.post("/upload", data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    try {
+      await axios.post(`/posts`, newPost);
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className='share'>
       <div className='shareWrapper'>
         <div className='shareTop'>
-          <img className='shareProfileImg' src='/assets/person/1.jpeg' alt='' />
-          <input className='shareInput' placeholder="What's in your mind?" />
+          <img
+            className='shareProfileImg'
+            src={
+              user.profilePicture
+                ? user.profilePicture
+                : process.env.REACT_APP_DEFAULT_PROFILE
+            }
+            alt=''
+          />
+          <input
+            className='shareInput'
+            placeholder={`What's on your mind, ${user.username
+              .charAt(0)
+              .toUpperCase()}${user.username.slice(1)}?`}
+            ref={desc}
+          />
         </div>
         <hr className='shareHr' />
-        <div className='shareBottom'>
+        <form className='shareBottom' onSubmit={submitHandler}>
           <div className='shareOptions'>
-            <div className='shareOption'>
+            <label htmlFor='fileID' className='shareOption'>
               <PermMedia htmlColor='tomato' className='shareIcon' />
               <span className='shareOptionText'>Media</span>
-            </div>
+              <input
+                style={{ display: "none" }}
+                type='file'
+                id='fileID'
+                accept='.png,.jpeg,.jpg'
+                onChange={(e) => setFile(e.target.files[0])}
+              />
+            </label>
             <div className='shareOption'>
               <Label htmlColor='blue' className='shareIcon' />
               <span className='shareOptionText'>Tag</span>
@@ -32,8 +86,10 @@ const Share = () => {
             </div>
           </div>
 
-          <button className='shareButton'>Share</button>
-        </div>
+          <button className='shareButton' type='submit'>
+            Share
+          </button>
+        </form>
       </div>
     </div>
   );
