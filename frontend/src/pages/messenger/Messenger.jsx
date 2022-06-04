9d03d6c8dd1406bb7091cca1ab075bcd6c65656c
LucidJoy <jyotirmoykarmakar02@gmail.com>
2022-06-04 09:46:20 +0530

    instant message using Socket.io

--- frontend/src/pages/messenger/Messenger.jsx
+++ frontend/src/pages/messenger/Messenger.jsx
@@ -1,11 +1,12 @@
 import React, { useContext, useEffect, useRef, useState } from "react";
+import axios from "axios";
+import { io } from "socket.io-client";
+
 import ChatOnline from "../../components/chatOnline/ChatOnline";
 import Conversation from "../../components/conversation/Conversation";
 import Message from "../../components/message/Message";
 import Topbar from "../../components/topbar/Topbar";
 import { AuthContext } from "../../context/AuthContext";
-import axios from "axios";
-
 import "./messenger.css";
 
 const Messenger = () => {
@@ -13,8 +14,34 @@ const Messenger = () => {
   const [currentChat, setCurrentChat] = useState(null);
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
+  const [arrivalMessage, setArrivalMessage] = useState(null);
   const { user } = useContext(AuthContext);
   const scrollRef = useRef();
+  const socket = useRef();
+
+  useEffect(() => {
+    socket.current = io("ws://localhost:8900");
+    socket.current.on("getMessage", (data) => {
+      setArrivalMessage({
+        sender: data.senderId,
+        text: data.text,
+        createdAt: Date.now(),
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    arrivalMessage &&
+      currentChat?.members.includes(arrivalMessage.sender) &&
+      setMessages((prev) => [...prev, arrivalMessage]);
+  }, [arrivalMessage, currentChat]);
+
+  useEffect(() => {
+    socket.current.emit("addUser", user?._id);
+    socket.current.on("getUsers", (users) => {
+      console.log(users);
+    });
+  }, [user]);
 
   useEffect(() => {
     const getConversations = async () => {
@@ -48,6 +75,14 @@ const Messenger = () => {
       conversationId: currentChat._id,
     };
 
+    const receiverId = currentChat.members.find((member) => member != user._id);
+
+    socket.current.emit("sendMessage", {
+      senderId: user._id,
+      receiverId,
+      text: newMessage,
+    });
+
     try {
       const res = await axios.post(`/messages`, message);
       setMessages([...messages, res.data]);
@@ -72,8 +107,8 @@ const Messenger = () => {
               placeholder='Search for friends'
               className='chatMenuInput'
             />
-            {conversations.map((convo) => (
-              <div onClick={() => setCurrentChat(convo)}>
+            {conversations.map((convo, i) => (
+              <div key={i} onClick={() => setCurrentChat(convo)}>
                 <Conversation conversation={convo} currentUser={user} />
               </div>
             ))}
@@ -85,8 +120,8 @@ const Messenger = () => {
             {currentChat ? (
               <>
                 <div className='chatBoxTop'>
-                  {messages.map((m) => (
-                    <div ref={scrollRef}>
+                  {messages.map((m, i) => (
+                    <div ref={scrollRef} key={i}>
                       <Message message={m} own={m.sender === user._id} />
                     </div>
                   ))}
